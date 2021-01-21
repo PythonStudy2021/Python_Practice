709ee3f2321a63f6e479e817596f586b3984e307
sson514 <76953561+HoJun-Song@users.noreply.github.com>
2021-01-21 22:45:39 +0900

    Add files via upload

---
+++ Ssong/test.py
@@ -0,0 +1,14 @@
+import game.sound.echo
+game.sound.echo.echo_test()
+
+from game.sound import echo
+echo.echo_test()
+
+from game.sound.echo import echo_test
+echo_test()
+
+from game.sound import*
+echo.echo_test()
+
+from game.graphic.render import render_test
+render_test()

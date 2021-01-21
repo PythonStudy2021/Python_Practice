67260399e91fb862a1f8c14dc3655f039c0d09fb
KANG HEE LEE <67950809+LeeKangh22@users.noreply.github.com>
2021-01-21 21:58:55 +0900

    Add files via upload

---
+++ Leekh22/Practice_module_1.py
@@ -0,0 +1,20 @@
+from mod1 import *
+
+num1=4
+num2=2
+
+a=add(num1,num2)
+print("4+2=%d"%a)
+
+b=sub(num1,num2)
+print(b)
+
+c=mul(num1,num2)
+print(c)
+
+d=div(num1,num2)
+print(d)
+
+e=pow(num1,num2)
+print(e)
+
---
+++ Leekh22/Practice_module_2.py
@@ -0,0 +1,7 @@
+import mod2
+
+print(mod2.PI)
+a=mod2.Math()
+
+b=a.solv(3)
+print(b)
